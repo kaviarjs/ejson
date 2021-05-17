861513d3b4faa5a60a8932c8577f1c02cf6b6d44
Theodor Diaconu <diaconu.theodor@gmail.com>
2021-05-17 09:16:21 +0300

    toModel fix

--- src/toModel.ts
+++ src/toModel.ts
@@ -3,5 +3,8 @@ type Constructor<T> = {
 };
 
 export function toModel<T>(className: Constructor<T>, data: Partial<T>) {
+  if (data === null || data === undefined) {
+    return null;
+  }
   return Object.assign(new className(), data);
 }
